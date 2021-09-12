21c88db9f99c141bc8c4f35d6441d99b78061402
ChenXiao Liu <810196813@qq.com>
2021-09-12 15:56:18 -0500

    Initial Commit

---
+++ ChenXiaoLiu-Lab1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChenXiaoLiu-Lab1
+//
+//  Created by lcx on 2021/9/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
